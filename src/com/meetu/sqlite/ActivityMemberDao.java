603c711e049a0338cb69a0346776abe8ca8e092c
lucifer <604324055@qq.com>
2015-12-28 10:30:11 +0800

    asd

--- src/com/meetu/sqlite/ActivityMemberDao.java
+++ src/com/meetu/sqlite/ActivityMemberDao.java
@@ -1,9 +1,19 @@
 package com.meetu.sqlite;
+
+import android.content.Context;
+
 /** 
  * @author  lucifer 
  * @date 2015-12-26
  * @return  
  */
 public class ActivityMemberDao {
+	private MySqliteDBHelper dbHelper;
+
+	public ActivityMemberDao(Context context) {
+		// TODO Auto-generated constructor stub
+		dbHelper = new MySqliteDBHelper(context);
+	}
+	
 
 }
